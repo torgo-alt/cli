509173a02342dc48f31ae77ceeb20db7e5637971
Craig Andrews <candrews@integralblue.com>
2022-09-27 10:54:44 -0400

    chore: upgrade to typescript 4.8
    Upgrade typescript from 3.9.10 to 4.8.4
    Includes necessary upgrades to these packages as well:
    @typescript-eslint/eslint-plugin
    @typescript-eslint/parser
    eslint
    eslint-config-prettier
    eslint-plugin-jest
    jest
    tap
    ts-jest
    ts-loader
    ts-node
    
    Signed-off-by: Craig Andrews <candrews@integralblue.com>

--- test/tap/cli-monitor.acceptance.test.ts
+++ test/tap/cli-monitor.acceptance.test.ts
@@ -69,7 +69,7 @@ if (!isWindows) {
     oldendpoint = key;
     t.pass('existing user endpoint captured');
 
-    await new Promise((resolve) => {
+    await new Promise<void>((resolve) => {
       server.listen(port, resolve);
     });
     t.pass('started demo server');
@@ -2022,7 +2022,7 @@ if (!isWindows) {
     delete process.env.SNYK_PORT;
     t.notOk(process.env.SNYK_PORT, 'fake env values cleared');
 
-    await new Promise((resolve) => {
+    await new Promise<void>((resolve) => {
       server.close(resolve);
     });
     t.pass('server shutdown');
